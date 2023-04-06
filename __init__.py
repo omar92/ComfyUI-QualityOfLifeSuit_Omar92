f16f003e004f617a2a3866fd8fb1e33b1cbc95a5
omar92 <ooomar92@gmail.com>
2023-04-06 22:35:32 +0200

    ready openAI API_key  from config file instead of separated file
    delete old api_key file + fix bug
    
    fix bug in config file path

--- __init__.py
+++ __init__.py
@@ -10,7 +10,8 @@ if not os.path.isfile(os.path.join(os.path.dirname(os.path.realpath(__file__)),"
     #create config file
     config = {
         "autoUpdate": True,
-        "branch": "dev"
+        "branch": "dev",
+        "openAI_API_Key": "sk-#########################################"
     }
     with open(os.path.join(os.path.dirname(os.path.realpath(__file__)),"config.json"), "w") as f:
         json.dump(config, f, indent=4)
